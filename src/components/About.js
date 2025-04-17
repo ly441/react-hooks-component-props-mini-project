0bbd24b391f3ef130f085b427b9ec8b7322a48e9
Lynn <koliilynn@gmail.com>
2025-04-17 18:21:27 +0300

    Refactor About component to use <aside> instead of <div> for semantic markup

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,12 @@ import React from 'react';
 function About({image="https://via.placeholder.com/215",about}){
 
     return(
-<div>
+
     <aside>
         <img src={image} alt="blog logo" />
         <p>{about}</p>
     </aside>
-</div>
+
     );
 }
 export default About;
